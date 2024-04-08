f6bf14d31d6383cb18ffaf04caa659162b54b77b
OpIranTech <mesbahmilad@gmail.com>
2024-04-08 15:39:09 -0700

    Fixing the missing directory

--- src/download_models.sh
+++ src/download_models.sh
@@ -2,5 +2,11 @@
 while IFS= read -r line; do
   url=$(echo $line | cut -d ' ' -f 1)
   file=$(echo $line | cut -d ' ' -f 2)
+  # Extract the directory path from $file
+  dir=$(dirname "/comfyui/$file")
+  
+  # Create the directory if it doesn't exist
+  mkdir -p "$dir"
+
   wget -q -O "/comfyui/$file" "$url"
 done < "$1"
